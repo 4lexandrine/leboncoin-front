4d7c47d9d20ef52483a83026eaf018a36ab02c93
Alexandrine Defoor <alexandrine.defoor@gmail.com>
2020-03-27 11:27:32 +0100

    updating react-scripts@3.4.0

--- src/components/CheckoutForm/CheckoutForm.js
+++ src/components/CheckoutForm/CheckoutForm.js
@@ -23,8 +23,8 @@ const CheckoutForm = ({ stripe, price, title, username, id }) => {
                     } else {
                         // console.log(stripeResponse.token);
 
-                        // const response = await axios.post(`https://leboncoin-4lexandrine.herokuapp.com/payment${id}`,
-                        const response = await axios.post(`http://localhost:3100/payment/${id}`,
+                        const response = await axios.post(`https://leboncoin-4lexandrine.herokuapp.com/payment${id}`,
+                            // const response = await axios.post(`http://localhost:3100/payment/${id}`,
                             {
                                 token: stripeResponse.token.id,
                                 price: price,
