fefd2ff7077597dafae83bf7a995fe3d36c16ec4
4lexandrine <alexandrine.defoor@gmail.com>
2020-09-18 12:47:36 +0200

    recaptcha

--- src/components/CheckoutForm/CheckoutForm.js
+++ src/components/CheckoutForm/CheckoutForm.js
@@ -1,39 +1,50 @@
 import React, { useState } from "react";
 import { CardElement, injectStripe } from "react-stripe-elements";
+import { GoogleReCaptchaProvider, GoogleReCaptcha } from 'react-google-recaptcha-v3';
 
 import axios from "axios";
 
 const CheckoutForm = ({ stripe, price, title, username }) => {
 
     const [purchaseComplete, setPurchaseComplete] = useState(false);
+    const [captchaToken, setCaptchaToken] = useState(null)
+
+    const onSubmit = async () => {
+        const stripeResponse = await stripe.createToken({ name: username });
+
+        if (stripeResponse.error) {
+            alert(stripeResponse.error.message);
+        } else {
+            const response = await axios.post(`https://leboncoin-4lexandrine.herokuapp.com/payment`,
+                {
+                    token: stripeResponse.token.id,
+                    price: price,
+                    title: title,
+                    captchaToken
+                }
+            );
+            if (response.status === 200) {
+                setPurchaseComplete(true);
+            } else {
+                alert("An error occurred");
+            }
+        }
+    }
 
     return (
         !purchaseComplete ? (
-            <div className="d-flex flex-column align-items">
-                {/* J'affiche mon formulmaire de CB */}
-                <CardElement className="card" />
-                {/* On envoie le numero de CB à Stripe, à aucun moment nous ne gérons la confidentialité ou la sécurité, c'est Stripe qui gère ! */}
-                <button className="orange-btn" onClick={async () => {
-                    const stripeResponse = await stripe.createToken({ name: username });
+            <GoogleReCaptchaProvider
+                reCaptchaKey="6LcPv80ZAAAAABIdojmtX5td4-JGbvnrSWaDIoux"
+            >
+                <GoogleReCaptcha onVerify={captchaToken => setCaptchaToken(captchaToken)} />
 
-                    if (stripeResponse.error) {
-                        alert(stripeResponse.error.message);
-                    } else {
-                        const response = await axios.post(`https://leboncoin-4lexandrine.herokuapp.com/payment`,
-                            {
-                                token: stripeResponse.token.id,
-                                price: price,
-                                title: title
-                            }
-                        );
-                        if (response.status === 200) {
-                            setPurchaseComplete(true);
-                        } else {
-                            alert("An error occurred");
-                        }
-                    }
-                }}>Valider</button>
-            </div >
+                <div className="d-flex flex-column align-items">
+                    {/* J'affiche mon formulmaire de CB */}
+                    <CardElement className="card" />
+                    {/* On envoie le numero de CB à Stripe, à aucun moment nous ne gérons la confidentialité ou la sécurité, c'est Stripe qui gère ! */}
+                    <button className="orange-btn" onClick={onSubmit}>Valider</button>
+                </div >
+            </GoogleReCaptchaProvider>
         ) : (
                 <div>
                     <h2>Paiement validé</h2>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 const Footer = () => {
     return (
         <footer className="d-flex justify-center align-items">
-            <p>Made with <a href="https://fr.reactjs.org/">React</a> at <a href="https://www.lereacteur.io/bootcamp-javascript/formation-developpeur-web-mobile/">Le Réacteur </a>by <a href="https://www.linkedin.com/in/alexandrine-d/">Me</a></p>
+            <p>Made with <a href="https://fr.reactjs.org/">React</a> at <a href="https://www.lereacteur.io/bootcamp-javascript/formation-developpeur-web-mobile/">Le Reacteur </a>by <a href="https://www.linkedin.com/in/alexandrine-d/">Alexandrine</a></p>
         </footer>
     );
 }
